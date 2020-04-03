b70ea5578e79c1f85e158a6221e4f7fbfcbd3d9c
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-03 09:27:01 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#code for the values
+print("Value 1:")
+a = int(input())
+print("Value 2:")
+b = int(input())
+print("Choose your operator (plus, minus, multiply, divide, or equals).")
+#calculation code
+op = input()
+if op == "plus":
+    print(a+b)
+elif op == "minus":
+    print(a-b)
+elif op == "multiply":
+    print(a*b)
+elif op == "divide":
+    print(a/b)
+elif op == "equals":
+    if (a==b):
+        print('true')
+    else:
+        print("false")
+#else code/error
+else:
+    print("Oops. You made a typo.")
