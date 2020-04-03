75667870b310115aa13e52f4d47aa382a79998c1
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-03 15:55:45 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,73 +1,64 @@
-print("Do you want 2 or 3 values?")
-z = input()
-if z == "Am I the dev?":
-    print('No.')
-elif z == 'two':
-        #code for the values
-    print("Value 1:"); a = int(input())
-    print("Value 2:"); b = int(input())
-    print("Choose your operator (+, -, *, /, or =).")
-    #calculation code
-    op = input()
-    if op == "+":
-        print(a+b+c)
-    elif op == "-":
-        print(a-b-c)
-    elif op == "*":
-        print(a*b*c)
-    elif op == "/":
-        print(a/b/c)
-    elif op == "=":
-        if (a==b):
-            print('True.')
-        elif (a>b):
-            print("False, a > b.")
-        else:
-            print('False, a < b.')
-    #else code/error
-    else:
-        print("Sorry about that. Looks like you made a typo. Try again.")
-elif z == '2':
-        #code for the values
-    print("Value 1:"); a = int(input())
-    print("Value 2:"); b = int(input())
-    print("Choose your operator (+, -, *, /, or =).")
-    #calculation code
-    op = input()
-    if op == "+":
-        print(a+b+c)
-    elif op == "-":
-        print(a-b-c)
-    elif op == "*":
-        print(a*b*c)
-    elif op == "/":
-        print(a/b/c)
-    elif op == "=":
-        if (a==b):
-            print('True.')
-        elif (a>b):
-            print("False, a > b.")
-        else:
-            print('False, a < b.')
-    #else code/error
-    else:
-        print("Sorry about that. Looks like you made a typo. Try again.")
+z = input("Do you want 2 or 3 values?: ").lower()
+if z in ("two", "2", "dos", "1+1"):
+    how_many = 2
+    print("You've picked: two values")
 else:
-    #code for the values
-    print("Value 1:"); a = int(input())
-    print("Value 2:"); b = int(input())
-    print("Value 3:"); c = int(input())
-    print("Choose your operator (+, -, *, or /).")
+    how_many = 3
+    print("You've picked: three values")
+
+while True:
+    try:
+        a = int(input("Value 1: "))
+        break
+    except ValueError:
+        print("Sorry about that. Looks like you made a typo. Try again.")
+while True:
+    try:
+        b = int(input("Value 2: "))
+        continue_it = True
+        break
+    except ValueError:
+        print("Sorry about that. Looks like you made a typo. Try again.")
+    
+if continue_it:
+    if how_many == 3:
+        while True:
+            try:
+                c = int(input("Value 3: "))
+                break
+            except ValueError:
+                print("Sorry about that. Looks like you made a typo. Try again.")
+    op = input("Choose your operator (plus, minus, multiply, divide, or equals): ")
     #calculation code
-    op = input()
-    if op == "+":
-        print(a+b+c)
-    elif op == "-":
-        print(a-b-c)
-    elif op == "*":
-        print(a*b*c)
-    elif op == "/":
-        print(a/b/c)
-    #else code/error
+    if op in ("plus", "+"):
+        if how_many == 2:
+            print(a+b)
+        else:
+            print(a+b+c)
+    elif op in ("minus", "-"):
+        if how_many == 2:
+            print(a-b)
+        else:
+            print(a-b-c)
+    elif op in ("multiply", "*"):
+        if how_many == 2:
+            print(a*b)
+        else:
+            print(a*b*c)
+    elif op in ("divide", "/"):
+        if how_many == 2:
+            print(a/b)
+        else:
+            print(a/b/c)
+    elif op in ("equals", "="):
+        if how_many == 2:
+            if (a==b):
+                print("True.")
+            elif (a>b):
+                print("False, a > b.")
+            else:
+                print("False, a < b.")
+        else:
+            print("Sorry about that. Looks like you made a typo. Try again.")
     else:
         print("Sorry about that. Looks like you made a typo. Try again.")
