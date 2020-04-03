114cd7a3ec796b84a5200162e04a13e720aaae24
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-03 14:12:58 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,31 +1,45 @@
-import sys
-from io import StringIO
-newin = StringIO("2\n1\n2\nplus\n")
-sys.stdin = newin
-
-def get_input():
-    try:
-        return input()
-    except EOFError:
-        return None
-
 print("Do you want 2 or 3 values?")
-z = get_input()
-if z == 'two' or z == '2':
-    #code for the values
-    print("Value 1:"); a = int(get_input())
-    print("Value 2:"); b = int(get_input())
+z = input()
+if z == 'two':
+        #code for the values
+    print("Value 1:"); a = int(input())
+    print("Value 2:"); b = int(input())
     print("Choose your operator (+, -, *, /, or =).")
     #calculation code
-    op = get_input()
+    op = input()
     if op == "+":
-        print(a+b)
+        print(a+b+c)
     elif op == "-":
-        print(a-b)
+        print(a-b-c)
     elif op == "*":
-        print(a*b)
+        print(a*b*c)
     elif op == "/":
-        print(a/b)
+        print(a/b/c)
+    elif op == "=":
+        if (a==b):
+            print('True.')
+        elif (a>b):
+            print("False, a > b.")
+        else:
+            print('False, a < b.')
+    #else code/error
+    else:
+        print("Sorry about that. Looks like you made a typo. Try again.")
+elif z == '2':
+        #code for the values
+    print("Value 1:"); a = int(input())
+    print("Value 2:"); b = int(input())
+    print("Choose your operator (+, -, *, /, or =).")
+    #calculation code
+    op = input()
+    if op == "+":
+        print(a+b+c)
+    elif op == "-":
+        print(a-b-c)
+    elif op == "*":
+        print(a*b*c)
+    elif op == "/":
+        print(a/b/c)
     elif op == "=":
         if (a==b):
             print('True.')
@@ -38,19 +52,19 @@ if z == 'two' or z == '2':
         print("Sorry about that. Looks like you made a typo. Try again.")
 else:
     #code for the values
-    print("Value 1:"); a = int(get_input())
-    print("Value 2:"); b = int(get_input())
-    print("Value 3:"); c = int(get_input())
-    print("Choose your operator (+, -, *, or ,).")
+    print("Value 1:"); a = int(input())
+    print("Value 2:"); b = int(input())
+    print("Value 3:"); c = int(input())
+    print("Choose your operator (+, -, *, or /).")
     #calculation code
-    op = get_input()
+    op = input()
     if op == "+":
         print(a+b+c)
     elif op == "-":
         print(a-b-c)
     elif op == "*":
         print(a*b*c)
-    elif op == ",":
+    elif op == "/":
         print(a/b/c)
     #else code/error
     else:
