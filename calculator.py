e020d192e1103598c2a919cd14cfa5e6cb443772
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-03 11:08:17 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,13 +6,13 @@ print("Choose your operator (plus, minus, multiply, divide, or equals).")
 #calculation code
 op = input()
 if op == "plus":
-    print(a+b)
+    print(a+b+c)
 elif op == "minus":
-    print(a-b)
+    print(a-b-c)
 elif op == "multiply":
-    print(a*b)
+    print(a*b*c)
 elif op == "divide":
-    print(a/b)
+    print(a/b/c)
 elif op == "equals":
     if (a==b):
         print('true')
