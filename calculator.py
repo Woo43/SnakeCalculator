85020288b1b32b540c1308a6e3aca37827b3a5a4
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-03 11:02:31 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 #code for the values
 print("Value 1:"); a = int(input())
 print("Value 2:"); b = int(input())
+print("Value 3 (optional, put 0 if adding/subtracting or 1 if multiplying/dividing doesn't work w/ equals):"); c = int(input())
 print("Choose your operator (plus, minus, multiply, divide, or equals).")
 #calculation code
 op = input()
