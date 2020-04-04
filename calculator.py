9f71343934c66339cbe0429c6dada83457a279fa
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-04 13:52:05 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,78 +1,87 @@
-z = input('Do you want 2 or 3 values? ')
+z = input("Do you want 2 or 3 values? ")
 if z == "Is Woo43 the dev?":
-    print('Yes.')
-elif z == 'two' or z == '2':
-        #code for the values
-    print("Value 1:"); a = int(input())
-    print("Value 2:"); b = int(input())
-    print("Choose your operator: + and plus, -  and minus, * and multiply,  / and divide, = and equals, sqrt a, sqrt b, cube a, cube b square a, or square b.")
-    #calculation code
+    print("Yes.")
+elif z == "two" or z == "2":
+    # code for the values
+    print("Value 1:")
+    a = int(input())
+    print("Value 2:")
+    b = int(input())
+    print(
+        "Choose your operator: + and plus, -  and minus, * and multiply,  / and divide, = and equals, sqrt a, sqrt b, cube a, cube b square a, or square b."
+    )
+    # calculation code
     op = input()
-    if op == "+" or op == 'plus':
-        print(a+b)
-    elif op == "-" or op == 'minus':
-        print(a-b)
-    elif op == "*" or op == 'multiply':
-        print(a*b)
-    elif op == "/" or op == 'divide':
-        print(a/b)
-    elif op == "=" or op == 'equals':
-        if (a==b):
-            print('True.')
-        elif (a>b):
+    if op == "+" or op == "plus":
+        print(a + b)
+    elif op == "-" or op == "minus":
+        print(a - b)
+    elif op == "*" or op == "multiply":
+        print(a * b)
+    elif op == "/" or op == "divide":
+        print(a / b)
+    elif op == "=" or op == "equals":
+        if a == b:
+            print("True.")
+        elif a > b:
             print("False, a > b.")
         else:
-            print('False, a < b.')
-    elif op == 'square a':
-        print(a*a)
-    elif op == 'square b':
-        print(b*b)
+            print("False, a < b.")
+    elif op == "square a":
+        print(a * a)
+    elif op == "square b":
+        print(b * b)
     elif op == "cube a":
-        print(a*a*a)
+        print(a * a * a)
     elif op == "cube b":
-        print(b*b*b)
+        print(b * b * b)
     elif op == "sqrt a":
         print(sqrt(a))
-    elif op == 'sqrt b':
+    elif op == "sqrt b":
         print(sqrt(b))
-    #else code/error
+    # else code/error
     else:
         print("Looks like you made a typo. Sorry about that.")
-elif z == 'three' or z == '3':
-    #code for the values
-    print("Value 1:"); a = int(input())
-    print("Value 2:"); b = int(input())
-    print("Value 3:"); c = int(input())
-    print("Choose your operator: + or plus, -  or minus, * or multiply,  / or divide, sqrt a, sqrt b, sqrt c square a, square b, square c, cube a, cube b, or cube c.")
-    #calculation code
+elif z == "three" or z == "3":
+    # code for the values
+    print("Value 1:")
+    a = int(input())
+    print("Value 2:")
+    b = int(input())
+    print("Value 3:")
+    c = int(input())
+    print(
+        "Choose your operator: + or plus, -  or minus, * or multiply,  / or divide, sqrt a, sqrt b, sqrt c square a, square b, square c, cube a, cube b, or cube c."
+    )
+    # calculation code
     op = input()
-    if op == "+" or op == 'plus':
-        print(a+b+c)
-    elif op == "-" or op == 'minus':
-        print(a-b-c)
-    elif op == "*" or op == 'multiply':
-        print(a*b*c)
-    elif op == "/" or op == 'divide':
-        print(a/b/c)
+    if op == "+" or op == "plus":
+        print(a + b + c)
+    elif op == "-" or op == "minus":
+        print(a - b - c)
+    elif op == "*" or op == "multiply":
+        print(a * b * c)
+    elif op == "/" or op == "divide":
+        print(a / b / c)
     elif op == "cube a":
-        print(a*a*a)
+        print(a * a * a)
     elif op == "cube b":
-        print(b*b*b)
+        print(b * b * b)
     elif op == "cube c":
-        print(c*c*c)
-    elif op == 'square a':
-        print(a*a)
-    elif op == 'square b':
-        print(b*b)
-    elif op == 'square c':
-        print(c*c)
+        print(c * c * c)
+    elif op == "square a":
+        print(a * a)
+    elif op == "square b":
+        print(b * b)
+    elif op == "square c":
+        print(c * c)
     elif op == "sqrt a":
         print(sqrt(a))
-    elif op == 'sqrt b':
+    elif op == "sqrt b":
         print(sqrt(b))
-    elif op == 'sqrt c':
+    elif op == "sqrt c":
         print(sqrt(c))
-    #jokular people error
+    # jokular people error
     else:
         print("Looks like you made a typo. Sorry about that.")
 else:
