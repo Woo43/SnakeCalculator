f143c6e317910e8210f3cb3b13c60a9c58a44de9
Woo43 <63087489+Woo43@users.noreply.github.com>
2020-04-03 20:07:35 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
+print('Do you want 2 or 3 values?')
 z = input()
 if z == "Am I the dev?":
     print('No.')
-if z == 'two':
-elif z == 'two':
+if z == 'two', or "2":
         #code for the values
     print("Value 1:"); a = int(input())
     print("Value 2:"); b = int(input())
@@ -27,32 +27,7 @@ elif z == 'two':
     #else code/error
     else:
         print("Sorry about that. Looks like you made a typo. Try again.")
-elif z == '2':
-        #code for the values
-    print("Value 1:"); a = int(input())
-    print("Value 2:"); b = int(input())
-    print("Choose your operator (+, -, *, /, or =).")
-    #calculation code
-    op = input()
-    if op == "+":
-        print(a+b+c)
-    elif op == "-":
-        print(a-b-c)
-    elif op == "*":
-        print(a*b*c)
-    elif op == "/":
-        print(a/b/c)
-    elif op == "=":
-        if (a==b):
-            print('True.')
-        elif (a>b):
-            print("False, a > b.")
-        else:
-            print('False, a < b.')
-    #else code/error
-    else:
-        print("Sorry about that. Looks like you made a typo. Try again.")
-else:
+elif z == 'three', or '3':
     #code for the values
     print("Value 1:"); a = int(input())
     print("Value 2:"); b = int(input())
@@ -71,3 +46,5 @@ else:
     #else code/error
     else:
         print("Sorry about that. Looks like you made a typo. Try again.")
+else:
+    print("Sorry about that. Looks like you made a typo. Try again.")
